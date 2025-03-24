1b73d6099875a4118d6fe9c0585d9fb904facac8
ansoln <ansoln@taltech.ee>
2025-03-24 18:37:26 +0200

    generate spring-boot project

---
+++ src/main/java/com/swedbank/accounts/TesttaskAccountsApiApplication.java
@@ -0,0 +1,13 @@
+package com.swedbank.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttaskAccountsApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TesttaskAccountsApiApplication.class, args);
+  }
+
+}
